91018102968fcfe47b2aaf01aca4b3af9d20665e
Elias Rayas <eliasrayasg@gmail.com>
2020-07-07 12:05:58 -0500

    Finalizado el diseño para desktop, se corrigen algunos detalles en la version mobile

--- src/script.js
+++ src/script.js
@@ -1,5 +1,34 @@
-//variables
+//selectores
 
-const prevButton = document.getElementById("previous")
-const nextButton = document.getElementById("next")
+//botones
+const $prevButton = document.getElementById("previous")
+const $nextButton = document.getElementById("next")
 
+
+const tanya = document.getElementsByClassName("first")
+const tanyaList = Array.from(tanya)
+const john = document.getElementsByClassName("second")
+const johnList = Array.from(john)
+
+
+//funciones
+
+const showElement = (element) => {
+  element.classList.toggle("hidden")
+}
+
+const showPersona = (persona) => {
+  persona.forEach( item => {
+    showElement(item)
+  })
+}
+
+$nextButton.addEventListener("click", () => {
+  showPersona(tanyaList)
+  showPersona(johnList)
+})
+
+$prevButton.addEventListener("click", () => {
+  showPersona(tanyaList)
+  showPersona(johnList)
+})
